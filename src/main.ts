975b4ec3cd7d8d1f542b55206548b1886f352914
たいち ひ <taichi221228@icloud.com>
2024-12-31 17:24:22 +0900

    feat: Add multiple geometries and circular arrangement logic
    Introduced various Three.js geometries and added them to the scene.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import "./style.css";
 import {
 	AmbientLight,
+	BoxGeometry,
 	Camera,
+	ConeGeometry,
+	CylinderGeometry,
 	DirectionalLight,
 	DoubleSide,
+	Mesh,
 	MeshStandardMaterial,
 	Object3D,
+	PlaneGeometry,
 	Scene,
+	SphereGeometry,
+	TetrahedronGeometry,
+	TorusGeometry,
 	Vector3,
 	WebGLRenderer,
 } from "three";
@@ -41,7 +49,24 @@ scene.add(directionalLight);
 const ambientLight = new AmbientLight(0x999999);
 scene.add(ambientLight);
 
+const geometries = [
+	new SphereGeometry(50),
+	new BoxGeometry(100, 100, 100),
+	new PlaneGeometry(100, 100),
+	new TetrahedronGeometry(100, 0),
+	new ConeGeometry(100, 100, 32),
+	new CylinderGeometry(50, 50, 100, 32),
+	new TorusGeometry(50, 30, 16, 100),
+];
+geometries.map((geometry, index) => {
+	const mesh = new Mesh(geometry, material);
+	container.add(mesh);
+	mesh.position.x = 400 * Math.sin((index / geometries.length) * Math.PI * 2);
+	mesh.position.z = 400 * Math.cos((index / geometries.length) * Math.PI * 2);
+});
+
 function tick() {
+	container.rotation.y += 0.01;
 	renderer.render(scene, camera);
 	requestAnimationFrame(tick);
 }
