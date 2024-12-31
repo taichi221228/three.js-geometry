965385a25e054b12306f97080f12a9cd37cd5200
たいち ひ <taichi221228@icloud.com>
2024-12-31 12:46:56 +0900

    Revert "feat: Remove unused assets, counter functionality, and styles"
    This reverts commit 757999e8c12bebaca33ca23b3f1db2b92d6b8f83.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
